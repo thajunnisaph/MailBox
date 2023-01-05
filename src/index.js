fb8fb69857eb4fabe1796b8379a3508ee1611ff5
thajunnisaph <thajunnisaph123@gmail.com>
2023-01-05 15:02:23 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import '../node_modules/react-bootstrap/dist/react-bootstrap'; //react bootstrap configuration
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
